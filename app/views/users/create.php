157717f05a00dab435d988f0cf4dd9588a9df016
NicoleAngelynAguba <nicoleaguba2@gmail.com>
2025-09-19 20:39:01 +0800

    update

--- app/views/users/create.php
+++ app/views/users/create.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Student Sign Up</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <link rel="stylesheet" href="<?=base_url();?>public/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body class="bg-gradient-to-br from-green-600 via-emerald-500 to-lime-400 min-h-screen flex items-center justify-center font-sans">
