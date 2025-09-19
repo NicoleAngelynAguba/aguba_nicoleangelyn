ed934e6f36c6d9e65eb2685a30655636d11c6ce3
NicoleAngelynAguba <nicoleaguba2@gmail.com>
2025-09-19 14:14:56 +0800

    update

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -42,7 +42,7 @@ class UsersController extends Controller {
             $total_rows,
             $records_per_page,
             $page,
-            site_url('') . '?q=' . urlencode($q)
+            site_url('/q') . urlencode($q)
         );
         $data['page'] = $this->pagination->paginate();
 
